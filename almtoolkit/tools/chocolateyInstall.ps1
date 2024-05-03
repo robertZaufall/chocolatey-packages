c40e037b3699ee676fe2de48c11b8428bc26afa4
robertZaufall <mail@zaufall.de>
2024-05-03 19:13:21 +0200

    add almtoolkit package

---
+++ almtoolkit/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+	packageName    = 'almtoolkit'
+	url            = 'https://github.com/microsoft/Analysis-Services/releases/download/5.0.44/AlmToolkitSetup.msi'
+	fileType       = 'msi'
+	silentArgs     = '/quiet /norestart'
+    checksum       = 'd88d3fc68d10bca4bd005bed21e0006b95e5d23d5a394413b6d3618e65f92307'
+    checksumType   = 'sha256'
+	validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ almtoolkit/update.ps1
@@ -0,0 +1,38 @@
+import-module chocolatey-au
+. $PSScriptRoot\..\_scripts\all.ps1
+
+$repo        = 'microsoft/Analysis-Services'
+
+$domain      = 'https://github.com'
+$releases    = $domain + '/' + $repo + '/releases'
+$assets      = $releases + '/expanded_assets'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"= "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"= "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksumType\s*=\s*)('.*')"= "`$1'$($Latest.ChecksumType32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases
+    $re_tag = "/tree/([0-9]+\.[0-9]+\.[0-9]+)$"
+    $tag_link = $download_page.links | ? href -match $re_tag | select -First 1 -expand href
+    $version = Split-Path $tag_link -Leaf
+
+    $assets_page = Invoke-WebRequest -Uri ($assets + '/' + $version)
+
+    $re  = ".*\.msi"
+    $url = $assets_page.links | ? href -match $re | select -First 1 -expand href
+    $url = $domain + $url
+
+    return @{
+        URL32        = $url
+        Version      = $version
+    }
+}
+
+update -ChecksumFor 32
