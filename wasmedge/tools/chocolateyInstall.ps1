34617a6511e3253fefef7747fddd77c2679070ae
robertZaufall <mail@zaufall.de>
2024-05-03 15:20:03 +0200

    add wasmedge

---
+++ wasmedge/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+	packageName    = 'wasmedge'
+	url            = 'https://github.com/WasmEdge/WasmEdge/releases/download/0.13.5/WasmEdge-0.13.5-windows.msi'
+	fileType       = 'msi'
+	silentArgs     = '/quiet /norestart'
+    checksum       = 'daed0b36666dc046d232b54bb8ed928b6c965063ca5f2a34f59b36d79b66fda7'
+    checksumType   = 'sha256'
+	validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
